48f487cc06f7943db253d1f47b057fc18740e1a7
dysha <dyha035@gmail.com>
2022-03-09 10:37:34 +0500

    add 1 test

---
+++ selenium_test/selenium_test/Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace selenium_test
+{
+    [TestFixture]
+    public class Tests
+    {
+        IWebDriver webDriver= new ChromeDriver();
+
+        [TestCase]
+        public void mainTitle()
+        {
+            webDriver.Url = "https://sibsutis.ru";
+            Assert.AreEqual("Сибирский государственный университет телекоммуникаций и информатики", webDriver.Title);
+            webDriver.Close();
+        }
+        [TearDown]
+        public void testEnd()
+        {
+            webDriver.Quit();
+        }
+    }
+}
